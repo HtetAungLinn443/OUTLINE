9068e4f749648950401d4179c87bb5bdf9300406
HtetAungLinn443 <htetaunglinn443@gmail.com>
2024-05-31 01:35:46 +0630

    31-5-2024 update

--- key.js
+++ key.js
@@ -1,6 +1,38 @@
 const keyArray = [
   {
+    country: "Japan",
+    key: "ss://YWVzLTI1Ni1nY206MzlvVWluWFZXN1FDUFU0OURid2RWbFMyRlpjTzNUTG91Z0orWUNycHpLQT0@jp-ss.ipracevpn.com:8443?type=tcp#JP",
+  },
+  {
     country: "Singapore",
-    key: "adsfdasf1",
+    key: "ss://ywvzlti1ni1ny206znfkmst0tjbnawdmalizttz5uxzmt25rulcrm3bsuexhmui1u2rurwryqt0@sg-ss.ipracevpn.com:8443?type=tcp#SG",
+  },
+  {
+    country: "Spain 1",
+    key: "ss://YWVzLTI1Ni1nY206WlNEOEhVZ0Uzd2NKN3NFa01yMXU2K2orUCtjREpXdGYrNG9ydFNWNnZsVT0@es-ss.ipracevpn.com:8443?type=tcp#Spain",
+  },
+  {
+    country: "Spain 2",
+    key: "ss://YWVzLTI1Ni1nY206MmYwMTMzNjUtZGQ0YS00ODU2LWJmNTEtNjI4NGExNTgwOTczQGVzLWZ1bGwucHJpdmF0ZWlwLm5ldDo4NDQz#Spain2",
+  },
+  {
+    country: "USA",
+    key: "ss://YWVzLTI1Ni1nY206M2VCNFpmTzNtcGtibDJCNklsQXdqcnA0UnU4anBHemp3VHRvTjlCbC8rUT0@us-ss.ipracevpn.com:8443?type=tcp#USA",
+  },
+  {
+    country: "Sweden",
+    key: "ss://YWVzLTI1Ni1nY206MWZiNjY3M2MtYmFkYS00NGNjLTk0YWMtYTk3NmM1Y2ViZDEyQHNlLWZ1bGwucHJpdmF0ZWlwLm5ldDo4NDQz#Sweden",
+  },
+  {
+    country: "India",
+    key: "ss://YWVzLTI1Ni1nY206K1RDUUs4MTIwQThnWWFWcGJncTFrK0s5WnZsNXB2aUdwYllVY2FZWU1HWT0@in-ss.ipracevpn.com:8443?type=tcp#India",
+  },
+  {
+    country: "Canada 1",
+    key: "ss://YWVzLTI1Ni1nY206cjJkVDcvdURMa1RtdTgwQ0h0MElEM3NXWkdBNjhUWU9lWVpzK3R1Q0Qvcz0@ca-ss.ipracevpn.com:8443?type=tcp#Canada 1",
+  },
+  {
+    country: "Canada 2",
+    key: "ss://YWVzLTI1Ni1nY206NTVhZDFhZTQtY2NlOC00Y2FjLWFhNTEtYjY1ZWFhMjZlYWQwQGNhLWZ1bGwucHJpdmF0ZWlwLm5ldDo4NDQz#Canada2",
   },
 ];
